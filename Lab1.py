8c15bb295666540b053a79ab7677ccdf7f6109ca
Jakub <jakubstefaniak99@gmail.com>
2024-05-20 20:24:36 +0200

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,27 @@
+import time
+
 def naive_method(text, pattern):
-    n = len(text)
+    n = len(text) 
     m = len(pattern)
     result = []
+    for i in range(n - m + 1):
+        match = True
+        for j in range(m):
+            if text[i + j] != pattern[j]:
+                match = False
+                break
+        if match:
+            result.append(i)
+    
+    return result
+
+text = "ababcabcababc"
+pattern = "abc"
+
+start = time.time()
+naive_result = naive_method(text, pattern)
+end = time.time()
+
+execution_time = end - start
+print("Naiwne wyszukiwanie:", naive_result)
+print(f"Czas wykonania: {execution_time:.6f} sekund")
